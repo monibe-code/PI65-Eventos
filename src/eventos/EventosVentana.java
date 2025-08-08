f371d736954e281393ac1700baa1b3d142de4bbf
Monica <hola.monibe@gmail.com>
2025-08-08 23:00:40 +0200

    Implementado método al minimizar Iconified la ventana

--- src/eventos/EventosVentana.java
+++ src/eventos/EventosVentana.java
@@ -20,15 +20,24 @@ class MarcoVentana extends JFrame{
 		setTitle("Respondiendo");
 		setBounds(300,300,500,350);
 		setVisible(true);
+		//creamos la instancia del oyente
+		M_Ventana oyenteVentana = new M_Ventana();
+		//preparamos el marco para recibir el oyente
+		addWindowListener(oyenteVentana);
+		
 	}
 }
 class M_Ventana implements WindowListener{
+	//tenemos que declarar todos aunque sólo usemos uno
 	public void windowActivated(WindowEvent e) {}
 	public void windowClosed(WindowEvent e) {}
 	public void windowClosing(WindowEvent e) {}
 	public void windowDeactivated(WindowEvent e) {}
 	public void windowDeiconified(WindowEvent e) {}
-	public void windowIconified(WindowEvent e) {}
+	public void windowIconified(WindowEvent e) {
+		//cuando minimicemos la ventana, se imprime en consola:
+		System.out.println("Ventana minimizada");
+	}
 	public void windowOpened(WindowEvent e) {}
 	
 	
