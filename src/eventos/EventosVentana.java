1b560e2c12b819c3a992939a651b468733eb826a
Monica <hola.monibe@gmail.com>
2025-08-08 22:56:20 +0200

    Todos los métodos de la interfaz WindowListener declarados

--- src/eventos/EventosVentana.java
+++ src/eventos/EventosVentana.java
@@ -1,6 +1,7 @@
 package eventos;
 
 import javax.swing.JFrame;
+import java.awt.event.*;
 
 public class EventosVentana {
 
@@ -20,4 +21,15 @@ class MarcoVentana extends JFrame{
 		setBounds(300,300,500,350);
 		setVisible(true);
 	}
+}
+class M_Ventana implements WindowListener{
+	public void windowActivated(WindowEvent e) {}
+	public void windowClosed(WindowEvent e) {}
+	public void windowClosing(WindowEvent e) {}
+	public void windowDeactivated(WindowEvent e) {}
+	public void windowDeiconified(WindowEvent e) {}
+	public void windowIconified(WindowEvent e) {}
+	public void windowOpened(WindowEvent e) {}
+	
+	
 }
