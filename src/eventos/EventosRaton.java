da50fcbf4a9c18ba8b082291b728ad4b448dd963
Monica <hola.monibe@gmail.com>
2025-08-12 06:38:20 +0200

    heredamos clase MouseAdapter y usamos algunos de sus métodos

--- src/eventos/EventosRaton.java
+++ src/eventos/EventosRaton.java
@@ -1,5 +1,6 @@
 package eventos;
 
+import java.awt.event.InputEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -29,11 +30,19 @@ class MarcoRaton extends JFrame{
 }
 
 class EventosDeRaton extends MouseAdapter{
-	public void mouseClicked(MouseEvent e) {
-		System.out.println("Has hecho click");
-	}
+//	public void mouseClicked(MouseEvent e) {
+//		//System.out.println("Coordenada X: "+ e.getX()+ "Coordenada Y: "+ e.getY());
+//		System.out.println(e.getClickCount());
+//	}
 	
-	public void mouseExited(MouseEvent e) {
-		System.out.println("Has salido");
+	public void mousePressed(MouseEvent e) {
+		//System.out.println(e.getModifiersEx());
+		if(e.getModifiersEx()==MouseEvent.BUTTON1_DOWN_MASK) {
+			System.out.println("Has pulsado el botón izquierdo del ratón");
+		}else if(e.getModifiersEx()==MouseEvent.BUTTON2_DOWN_MASK) {
+			System.out.println("Has pulsado la rueda del ratón");
+		}else if(e.getModifiersEx()==4096) {
+			System.out.println("Has pulsado el botón DERECHO del ratón");
+		}
 	}
 }
