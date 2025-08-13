965fb3ba3a057468bb2302daa7fd34535d975b85
Monica <hola.monibe@gmail.com>
2025-08-13 13:03:49 +0200

    comprobamos si un texto tiene una arroba y lanzamos mensaje por pantalla cuando el cuadro de texto pierde el foco. Eventos de Foco de componentes

--- src/eventos/FocoEvento.java
+++ src/eventos/FocoEvento.java
@@ -64,7 +64,18 @@ class LaminaFoco extends JPanel{
 		@Override
 		public void focusLost(FocusEvent e) {
 			// TODO Auto-generated method stub
-			System.out.println("He perdido el foco");
+			String email= cuadro1.getText();
+			boolean comprobacion = false; 
+			for (int i=0; i<email.length();i++) {
+				if(email.charAt(i)=='@') {
+					comprobacion = true;
+				}
+			}
+			if(comprobacion) {
+				System.out.println("Correcto");
+			}else {
+				System.out.println("Email incorrecto");
+			}
 		}
 		
 	}
